254d01491191561d8b81d9ebd1a1b07c7f23a626
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2026-04-28 12:30:44 +0300

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import loader from "@/public/animations/Money_loading.json";
 export default function LoadingPage() {
     return (
         <div className="fixed inset-0 flex flex-col items-center justify-center bg-background/50 backdrop-blur-sm z-[9999]">
-            <div className="w-28 h-28 md:w-32 md:h-32 lg:w-40 lg:h-40">
+            <div className="w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32">
                 <Lottie
                     animationData={loader}
                     loop={true}
